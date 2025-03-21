bf3da9f200aaddfcf699048c485da1259adb7c81
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2025-03-21 13:49:34 +0000

    fewojfpiewijfewpi cluster

--- matrices/laplacian/julia-laplaciano.jl
+++ matrices/laplacian/julia-laplaciano.jl
@@ -8,17 +8,17 @@ nx = N
 ny = N
 nz = N
 
-ex = fill(1, nx)
-ey = fill(1, ny)
-ez = fill(1, nz)
+ex = fill(1, nx - 1)
+ey = fill(1, ny - 1)
+ez = fill(1, nz - 1)
 
-Dxx = spdiagm(-1 => ex, 0 => -2 * ex, +1 => ex)
-Dyy = spdiagm(-1 => ey, 0 => -2 * ey, +1 => ey)
-Dzz = spdiagm(-1 => ez, 0 => -2 * ez, +1 => ez)
+Dxx = spdiagm(-1 => ex, 0 => -2 * [ex; 1], +1 => ex)
+Dyy = spdiagm(-1 => ey, 0 => -2 * [ey; 1], +1 => ey)
+Dzz = spdiagm(-1 => ez, 0 => -2 * [ez; 1], +1 => ez)
 
-Ix = spdiagm(0 => [ex; 1])
-Iy = spdiagm(0 => [ey; 1])
-Iz = spdiagm(0 => [ez; 1])
+Ix = spdiagm(0 => ex)
+Iy = spdiagm(0 => ey)
+Iz = spdiagm(0 => ez)
 
 L = kron(Dxx, Iy, Iz) + kron(Ix, Dyy, Iz) + kron(Ix, Iy, Dzz)
 
