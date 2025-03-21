430d853f772ae4720877ae575c1a1aa609ce7372
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2025-03-21 13:59:51 +0000

    fjewpijfepw cluster 2

--- matrices/laplacian/julia-laplaciano.jl
+++ matrices/laplacian/julia-laplaciano.jl
@@ -16,9 +16,9 @@ Dxx = spdiagm(-1 => ex, 0 => -2 * [ex; 1], +1 => ex)
 Dyy = spdiagm(-1 => ey, 0 => -2 * [ey; 1], +1 => ey)
 Dzz = spdiagm(-1 => ez, 0 => -2 * [ez; 1], +1 => ez)
 
-Ix = spdiagm(0 => ex)
-Iy = spdiagm(0 => ey)
-Iz = spdiagm(0 => ez)
+Ix = spdiagm(0 => [ex; 1])
+Iy = spdiagm(0 => [ey; 1])
+Iz = spdiagm(0 => [ez; 1])
 
 L = kron(Dxx, Iy, Iz) + kron(Ix, Dyy, Iz) + kron(Ix, Iy, Dzz)
 
